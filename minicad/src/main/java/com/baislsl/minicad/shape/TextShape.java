a58aff0b97fbe1c7135c9f8b0cd4ed102ed1b497
baislsl <baislsl666@gmail.com>
2017-12-30 00:25:58 +0800

    text shape resize

--- minicad/src/main/java/com/baislsl/minicad/shape/TextShape.java
+++ minicad/src/main/java/com/baislsl/minicad/shape/TextShape.java
@@ -16,7 +16,7 @@ import org.slf4j.LoggerFactory;
 
 public class TextShape extends AbstractShape {
     private final static Logger log = LoggerFactory.getLogger(TextShape.class);
-    private String text = "input_text";
+    private String text = "text";
 
     TextShape(DrawBoard canvas, int x1, int y1, int x2, int y2) {
         super(canvas);
@@ -32,8 +32,11 @@ public class TextShape extends AbstractShape {
 
     @Override
     protected void onFeaturePointDrag(MouseEvent e) {
-        currentPoint.x = e.x;
-        currentPoint.y = e.y;
+        int x = currentPoint.x, y = currentPoint.y;
+        for(Point p : featurePoints){
+            if(p.x == x) p.x = e.x;
+            if(p.y == y) p.y = e.y;
+        }
     }
 
     private void onOpenTextPanel() {
@@ -70,20 +73,19 @@ public class TextShape extends AbstractShape {
 
 
     private void adjustToText(GC gc) {
-        // TODO:
         int x1 = featurePoints.get(0).x, y1 = featurePoints.get(0).y,
                 x2 = featurePoints.get(1).x, y2 = featurePoints.get(1).y;
-        Point textSize = gc.textExtent(text);
         FontData fontData = gc.getFont().getFontData()[0];
-        double rate = 1.0 * Math.abs(y2 - y1) / textSize.y;
-        fontData.setHeight(Math.abs(y2 - y1));
+        Point dpi = gc.getDevice().getDPI();    // 每英寸x，y偏移量
+        fontData.setHeight((y2 - y1)* dpi.y/144);
+        gc.setFont(new Font(drawBoard.getCanvas().getDisplay(), fontData));
+        Point textSize = gc.textExtent(text);
 
         featurePoints.get(0).x = Math.min(x1, x2);
         featurePoints.get(0).y = Math.min(y1, y2);
-        featurePoints.get(1).x = Math.min(x1, x2) + (int) (textSize.x * rate);
-        featurePoints.get(1).y = Math.min(y1, y2) + (int) (textSize.y * rate);
+        featurePoints.get(1).x = Math.min(x1, x2) +  textSize.x;
+        featurePoints.get(1).y = Math.min(y1, y2) + textSize.y;
 
-        gc.setFont(new Font(drawBoard.getCanvas().getDisplay(), fontData));
     }
 
     @Override
